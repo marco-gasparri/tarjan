a64a3fbb858957f0d9cc12096c35bc6c648cfdcd
Marco Gasparri <marco.gasparri@gmail.com>
2014-12-14 19:45:02 +0100

    Create helper.h

---
+++ helper.h
@@ -0,0 +1,13 @@
+#ifndef HELP_H
+#define HELP_H
+
+#include <stdio.h> // printf()
+#include <stdlib.h> // exit()
+#include <math.h>
+//#include <malloc.h>;
+
+
+int    *I_alloc (int);
+int    **PI_alloc(int);
+
+#endif
